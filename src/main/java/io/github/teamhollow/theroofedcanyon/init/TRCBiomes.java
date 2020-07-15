470492883e00aecff766b3e729e629e1e69014cc
Daniel Orr <orrdaniel73@gmail.com>
2020-07-15 16:33:13 +0100

    Create base biome

--- src/main/java/io/github/teamhollow/theroofedcanyon/init/TRCBiomes.java
+++ src/main/java/io/github/teamhollow/theroofedcanyon/init/TRCBiomes.java
@@ -7,7 +7,11 @@ import net.minecraft.util.registry.Registry;
 import net.minecraft.world.biome.Biome;
 
 public class TRCBiomes {
-    public static final Biome ROOFED_CANYON = Registry.register(Registry.BIOME, new Identifier(TheRoofedCanyon.MOD_ID, "roofed_canyon"), new RoofedCanyonBiome(null));
+    public static final Biome ROOFED_CANYON = register("roofed_canyon", new RoofedCanyonBiome());
 
     public TRCBiomes() {}
+
+    private static Biome register(String id, Biome biome) {
+        return Registry.register(Registry.BIOME, new Identifier(TheRoofedCanyon.MOD_ID, id), biome);
+    }
 }
