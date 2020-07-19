11c14d65cd0322a4f782df04f69cd9db7b370a88
Daniel Orr <orrdaniel73@gmail.com>
2020-07-19 20:44:24 +0100

    Increase Turfwood Tree max water depth & add vines

--- src/main/java/io/github/teamhollow/theroofedcanyon/init/TRCDecorators.java
+++ src/main/java/io/github/teamhollow/theroofedcanyon/init/TRCDecorators.java
@@ -10,6 +10,7 @@ import net.minecraft.util.registry.Registry;
 import net.minecraft.world.gen.decorator.AlterGroundTreeDecorator;
 import net.minecraft.world.gen.decorator.Decorator;
 import net.minecraft.world.gen.decorator.DecoratorConfig;
+import net.minecraft.world.gen.decorator.LeaveVineTreeDecorator;
 import net.minecraft.world.gen.decorator.NopeDecoratorConfig;
 import net.minecraft.world.gen.feature.TreeFeatureConfig;
 import net.minecraft.world.gen.feature.size.TwoLayersFeatureSize;
@@ -20,7 +21,7 @@ import net.minecraft.world.gen.trunk.DarkOakTrunkPlacer;
 public class TRCDecorators {
     public static WoodBlocks TURFWOOD = TRCBlocks.TURFWOOD;
 
-    public static final TreeFeatureConfig TURFWOOD_TREE_CONFIG = new TreeFeatureConfig.Builder(new SimpleBlockStateProvider(TURFWOOD.LOG.getDefaultState()), new SimpleBlockStateProvider(TURFWOOD.LEAVES.getDefaultState()), new DarkOakFoliagePlacer(8, 2, 0, 0), new DarkOakTrunkPlacer(6, 2, 1), new TwoLayersFeatureSize(1, 1, 2)).decorators(ImmutableList.of(new AlterGroundTreeDecorator(new SimpleBlockStateProvider(TURFWOOD.LEAVES.getDefaultState())))).build();
+    public static final TreeFeatureConfig TURFWOOD_TREE_CONFIG = new TreeFeatureConfig.Builder(new SimpleBlockStateProvider(TURFWOOD.LOG.getDefaultState()), new SimpleBlockStateProvider(TURFWOOD.LEAVES.getDefaultState()), new DarkOakFoliagePlacer(8, 2, 0, 0), new DarkOakTrunkPlacer(6, 2, 1), new TwoLayersFeatureSize(1, 1, 2)).maxWaterDepth(3).decorators(ImmutableList.of(new AlterGroundTreeDecorator(new SimpleBlockStateProvider(TURFWOOD.LEAVES.getDefaultState())), LeaveVineTreeDecorator.field_24961)).build();
 
     public static final Decorator<NopeDecoratorConfig> TURFWOOD_TREE = register("turfwood_tree", new TurfwoodTreeDecorator(NopeDecoratorConfig.field_24891));
 
