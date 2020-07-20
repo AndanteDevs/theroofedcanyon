c32d804c5fbfebe11e1172fc99a637a83fc44c97
Daniel Orr <orrdaniel73@gmail.com>
2020-07-20 11:31:45 +0100

    Make Vile particles green

--- src/main/java/io/github/teamhollow/theroofedcanyon/init/TRCParticleTypes.java
+++ src/main/java/io/github/teamhollow/theroofedcanyon/init/TRCParticleTypes.java
@@ -1,20 +1,24 @@
 package io.github.teamhollow.theroofedcanyon.init;
 
 import io.github.teamhollow.theroofedcanyon.TheRoofedCanyon;
+import io.github.teamhollow.theroofedcanyon.client.particle.BlockLeakParticle.*;
 import io.github.teamhollow.theroofedcanyon.particle.PublicDefaultParticleType;
 import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
-import net.minecraft.client.particle.BlockLeakParticle.DrippingHoneyFactory;
 import net.minecraft.particle.DefaultParticleType;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class TRCParticleTypes {
     public static final DefaultParticleType DRIPPING_VILE = register("dripping_vile", false);
+	public static final DefaultParticleType FALLING_VILE = register("falling_vile", false);
+	public static final DefaultParticleType LANDING_VILE = register("landing_vile", false);
 
     public TRCParticleTypes() {}
     
     public static void registerFactories() {
-        ParticleFactoryRegistry.getInstance().register(DRIPPING_VILE, DrippingHoneyFactory::new);
+        ParticleFactoryRegistry.getInstance().register(DRIPPING_VILE, DrippingVileFactory::new);
+        ParticleFactoryRegistry.getInstance().register(FALLING_VILE, FallingVileFactory::new);
+        ParticleFactoryRegistry.getInstance().register(LANDING_VILE, LandingVileFactory::new);
     }
 
     private static DefaultParticleType register(String id, boolean alwaysShow) {
